c6e0359bd4a4abb40d2d9234aff2105e2de78242
RODF10 <blueapp2015@hotmail.com>
2024-11-06 16:14:38 -0600

    Cambio V2.1

--- src/app/Layout/header/header.component.ts
+++ src/app/Layout/header/header.component.ts
@@ -1,22 +1,35 @@
-import { Component, HostListener } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { LoadJSService } from 'src/app/Service/load-js.service';
+import { NavbarService } from 'src/app/Service/navbar.service';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit{
 
   dropdownOpen = false;
 
-  constructor(private LoadJS: LoadJSService, private router: Router){
+  constructor(private LoadJS: LoadJSService, private router: Router, public varService: NavbarService){
     LoadJS.Carga(["Profile"]);
   }
 
   mainPage(){
     this.router.navigate(['login/dashboard']);
+  }
+
+  ngOnInit() {
+    /*this.router.events.subscribe(event => {
+      if (event instanceof NavigationEnd) {
+        if (event.url === '/login') {
+          this.varService.hide();
+        } else {
+          this.varService.show();
+        }
+      }
+    });*/
   }
 
 }
---
+++ src/app/Service/navbar.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class NavbarService {
+  public visible: boolean = false;
+
+  constructor() {}
+
+  show() {
+    this.visible = true;
+  }
+
+  hide() {
+    this.visible = false;
+  }
+}
