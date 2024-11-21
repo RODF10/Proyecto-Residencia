4cfa7b3a70dde3e159f65d33f3a7e83941b25ca2
Fernandochan28 <7010@itescam.edu.mx>
2024-11-20 23:24:35 -0600

    actualizacion cornell

--- src/app/Ventanas/Diagnosticos/Afectiva/encuesta-cornell/encuesta-cornell.component.ts
+++ src/app/Ventanas/Diagnosticos/Afectiva/encuesta-cornell/encuesta-cornell.component.ts
@@ -8,16 +8,25 @@ import { Router } from '@angular/router';
 })
 export class EncuestaCornellComponent {
   questions = [
-    { text: 'Tristeza', score: 0 },
-    { text: 'Llanto', score: 0 },
-    { text: 'Pérdida de interés', score: 0 },
-    { text: 'Agitación psicomotora', score: 0 },
-    { text: 'Pérdida de peso', score: 0 },
-    { text: 'Insomnio', score: 0 },
-    { text: 'Fatiga', score: 0 },
-    { text: 'Sentimientos de inutilidad', score: 0 },
-    { text: 'Dificultad para concentrarse', score: 0 },
-    { text: 'Ideación suicida', score: 0 },
+    { text: 'Ansiedad (Expresion ansiosa, rumiacion de ideas, preocupacion)', score: 0 },
+    { text: 'Tristesa (Expresion triste, voz apagada, tendencia al llanto)', score: 0 },
+    { text: 'Falta de reactividad a los acontesimientos placenteros', score: 0 },
+    { text: 'irritabilidad (facilmente enojable, poco temperamento)', score: 0 },
+    { text: 'Agitacion (incapacidad de permanecer quieto, jugar con las manos, tirarse el pelo)', score: 0 },
+    { text: 'Enlentecimiento (Movimientos, habla y reacciones enlentecidos)', score: 0 },
+    { text: 'Quejas Fisicas multiples (puntue 0 si son unicamente gastrointestinales)', score: 0 },
+    { text: 'perdida del interes (menos envuelto en las actividades habituales; puntue solamente si el cambio ha sido agudo, p. ej., en menos de 1 mes)', score: 0 },
+    { text: 'Perdida de apetito (come menos de lo habitual)', score: 0 },
+    { text: 'Perdida de peso (puntue 2 si 2.5kg en 1 mes )', score: 0 },
+    { text: 'Perdida de energia (Se fatiga facilmente, incapaz de mantener actividades; puntue solamente si el cambio ha sido agudo, p. ej., en menos de 1 mes )', score: 0 },
+    { text: 'variaciones diurnas del estado de animo (los sintomas empeoran por la mañana)', score: 0 },
+    { text: 'Dificultad para conciliar el sueño (mas de lo habitual para el paciente ) ', score: 0 },
+    { text: 'Despertares multiples durante el sueño', score: 0 },
+    { text: 'Despertar precoz (antes de lo habitual para el paciente)', score: 0 },
+    { text: 'Suicidio (siente que la vida no merece ser vivida, tiene deseos suicidas o realiza intentos de suicidio) ', score: 0 },
+    { text: 'Baja autoestima (autoculpa, autodepreciacion, sentimiento de fracaso)', score: 0 },
+    { text: 'Pesimismo (anticipacion a lo peor)', score: 0 },
+    { text: 'Delirios congruentes con el estado de ánimo (delirios de pobresa, enfermedades o pérdidas)', score: 0 },
   ];
 
   resultado: number = 0;
@@ -33,12 +42,10 @@ export class EncuestaCornellComponent {
   calculateScore(): void {
     this.resultado = this.questions.reduce((sum, question) => sum + question.score, 0);
 
-    if (this.resultado <= 6) {
-      this.observacion = 'Sin depresión o leve';
-    } else if (this.resultado <= 10) {
-      this.observacion = 'Depresión moderada';
-    } else {
-      this.observacion = 'Depresión severa';
+    if (this.resultado <= 12) {
+      this.observacion = 'depresion (Menor o mayor) ';
+    } else if (this.resultado <= 38 ) {
+      this.observacion = 'Depresión mayor';
     }
 
     this.showErrors = true; // Activa la validación para enviar resultados.
@@ -57,7 +64,7 @@ export class EncuestaCornellComponent {
         nombreEncuesta: 'Escala de Cornell',
         puntaje: this.resultado,
         observacion: this.observacion,
-        porcentaje: ((this.resultado / 30) * 100).toFixed(2) // Calcula un porcentaje basado en un puntaje máximo de 30.
+        porcentaje: ((this.resultado / 38) * 100).toFixed(2) // Calcula un porcentaje basado en un puntaje máximo de 30.
       }
     });
   }
